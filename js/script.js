66bf5d3ed457b7097f8f5031b291f407730989ad
aldeevsk <59862247+aldeevsk@users.noreply.github.com>
2021-03-16 15:13:15 +0600

    css and js links edit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 /* Mobile nav */
 
 const burger = document.querySelector('.burger');
+const mobileNav = document.querySelector('.mobile-nav');
+const navTitle = document.querySelector('.mobile-nav-title');
 
 burger.addEventListener('click', function () {
     burger.classList.toggle('active');
+    mobileNav.classList.toggle('active');
+    navTitle.classList.toggle('active');
 });
+
